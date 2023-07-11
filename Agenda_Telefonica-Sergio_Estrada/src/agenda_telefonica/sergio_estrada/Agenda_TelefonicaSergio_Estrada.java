e06234f334c82122cffb7d9b2a6fdf0aff482ac4
sergioestra <sergioabrahamestradaoroxon74@gmail.com>
2023-07-11 13:17:30 -0600

    creacion de menu y contacto

---
+++ Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/Agenda_TelefonicaSergio_Estrada.java
@@ -0,0 +1,24 @@
+
+package agenda_telefonica.sergio_estrada;
+
+import java.util.Scanner;
+
+public class Agenda_TelefonicaSergio_Estrada {
+
+    public static void main(String[] args) {
+      int opc = 0;
+      Scanner Estrada = new Scanner(System.in);
+        while (op!=4){
+        System.out.println("-----MENU-----");
+        System.out.println("1.Guardar Contacto");
+        System.out.println("2.Ver Todos Los Contactos");
+        System.out.println("3.Buscar Contacto");
+        System.out.println("4.Salir");
+        System.out.println("--------------------------------------------------");
+        System.out.println("INGRESE UN NUMERO DE OPCION");
+        System.out.println("--------------------------------------------------");
+        opc = Estrada.nextInt();
+        }
+    }
+    
+}
---
+++ Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/contacto.java
@@ -0,0 +1,12 @@
+
+package agenda_telefonica.sergio_estrada;
+
+public class contacto {
+   private String numero_telefono,nombre,adomicilio; 
+   
+  public contacto(String numero_telefono, String nombre, String adomicilio) {
+        this.numero_telefono = numero_telefono;
+        this.nombre = nombre;
+        this.adomicilio = adomicilio;
+    } 
+}
