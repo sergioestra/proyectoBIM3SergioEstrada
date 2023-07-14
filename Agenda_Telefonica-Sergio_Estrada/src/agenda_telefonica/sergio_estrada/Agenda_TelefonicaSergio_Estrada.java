6ae2835962943c4653300cc1e8bba4fc60faf6e1
sergio abraham estrada oroxon <138618405+sergioestra@users.noreply.github.com>
2023-07-13 20:32:55 -0500

    CORREJIR ERRORES DEL PRIMER COMIT

--- Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/Agenda_TelefonicaSergio_Estrada.java
+++ Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/Agenda_TelefonicaSergio_Estrada.java
@@ -8,7 +8,7 @@ public class Agenda_TelefonicaSergio_Estrada {
     public static void main(String[] args) {
       int opc = 0;
       Scanner Estrada = new Scanner(System.in);
-        while (op!=4){
+        while (opc!=4){
         System.out.println("-----MENU-----");
         System.out.println("1.Guardar Contacto");
         System.out.println("2.Ver Todos Los Contactos");
