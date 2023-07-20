88522db2dcd7a0857f1e900860ab895c8169578c
sergioestra <sergioabrahamestradaoroxon74@gmail.com>
2023-07-19 18:26:31 -0600

    finaliza por completo la clase contacto

--- Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/contacto.java
+++ Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/contacto.java
@@ -2,11 +2,34 @@
 package agenda_telefonica.sergio_estrada;
 
 public class contacto {
-   private String numero_telefono,nombre,adomicilio; 
-   
-  public contacto(String numero_telefono, String nombre, String adomicilio) {
-        this.numero_telefono = numero_telefono;
-        this.nombre = nombre;
-        this.adomicilio = adomicilio;
-    } 
+   private String numero_telefono,nombre,adomicilio;
+
+public contacto(String numero_telefono, String nombre, String adomicilio) {
+
+this.numero_telefono = numero_telefono;
+
+this.nombre = nombre;
+
+this.adomicilio = adomicilio;
+
+}
+
+public String vernum_tel(){
+
+return this.numero_telefono;
+
+}
+
+public String vercontacto(){
+
+return this.nombre;
+
+}
+
+public String veradomicilio (){
+
+return this.adomicilio;
+
+}
+
 }
