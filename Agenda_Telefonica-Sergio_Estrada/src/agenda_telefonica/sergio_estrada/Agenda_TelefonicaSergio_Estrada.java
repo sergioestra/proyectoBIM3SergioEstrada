c4b3aa368cbfc4783c479a356c9bbdf2d71bd7d2
sergioestra <sergioabrahamestradaoroxon74@gmail.com>
2023-07-21 13:25:19 -0600

    PROYECTO DE TERCER BLOQUE TERMINADO

--- Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/Agenda_TelefonicaSergio_Estrada.java
+++ Agenda_Telefonica-Sergio_Estrada/src/agenda_telefonica/sergio_estrada/Agenda_TelefonicaSergio_Estrada.java
@@ -4,100 +4,145 @@ import java.util.Scanner;
 
 public class Agenda_TelefonicaSergio_Estrada {
 
-    static contacto miscontactos[] = new contacto[15];
+   static contacto miscontactos[] = new contacto[15];
 
-    static int contador = 0;
+static int contador = 0;
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        int opc = 0;
+int opc = 0;
 
-        Scanner teclado = new Scanner(System.in);
+Scanner teclado = new Scanner(System.in);
 
-        while (opc != 4) {
+while (opc != 4) {
 
-            System.out.println("---------------------Menu principal---------------------");
+System.out.println("---------------------Menu principal---------------------");
 
-            System.out.println("1. Crear contacto");
+System.out.println("1. Crear contacto");
 
-            System.out.println("2. Ver contacto");
+System.out.println("2. Ver contacto");
 
-            System.out.println("3. Buscar contacto");
+System.out.println("3. Buscar contacto");
 
-            System.out.println("4. Salir");
+System.out.println("4. Salir");
 
-            System.out.println("Ingrese el numero de opcion");
+System.out.println("Ingrese el numero de opcion");
 
-            opc = teclado.nextInt();
+opc = teclado.nextInt();
 
-            if (opc == 1) {
+if (opc == 1) {
 
-                crearcontacto();
+crearcontacto();
 
-            } else if (opc == 2) {
+} else if (opc == 2) {
 
-                System.out.println("--Los datos del contacto son--");
+System.out.println("--Los datos del contacto son--");
 
-                verTodos();
+verTodos();
 
-            } 
-            
+} else if (opc == 3) {
 
-                
-        }
+System.out.println("--Buscar contacto--");
 
-    }
+System.out.println("ingrese su numero de telefono : ");
 
-    static public void verTodos() {
+Scanner entrada = new Scanner(System.in);
 
-        for (int i = 0; i < contador; i++) {
+String contacto = teclado.nextLine();
 
-            System.out.println("nombre: " + miscontactos[i].vercontacto());
+System.out.println(vercontacto(contacto));
 
-            System.out.println("numero de telefono: " + miscontactos[i].vernum_tel());
+} else if (opc == 4) {
 
-            System.out.println("adomicilio: " + miscontactos[i].veradomicilio());
+System.out.println("--Programa finalizado--");
 
-            System.out.println("#####################################################");
+break;
 
-        }
+} else {
 
-    }
+System.out.println("Opcion incorrecta");
 
-    static public void crearcontacto() {
+}
 
-        if (contador >= 15) {
+}
 
-            System.out.println("--Memoria llena--");
+}
 
-        } else {
+static public void verTodos() {
 
-            Scanner entrada = new Scanner(System.in);
+for (int i = 0; i < contador; i++) {
 
-            System.out.println("-------Contacto Nuevo-------");
+System.out.println("nombre: " + miscontactos[i].vercontacto());
 
-            System.out.println("-Ingrese el nombre del contacto");
+System.out.println("numero de telefono: " + miscontactos[i].vernum_tel());
 
-            String nombre1 = entrada.nextLine();
+System.out.println("adomicilio: " + miscontactos[i].veradomicilio());
 
-            System.out.println("-Ingrese su numero de telefono");
+System.out.println("#####################################################");
 
-            String num_tel = entrada.nextLine();
+}
 
-            System.out.println("-ingrese su adomicilio-");
+}
 
-            String adomicilio1 = entrada.nextLine();
+static public void crearcontacto() {
 
-            miscontactos[contador] = new contacto(num_tel, nombre1, adomicilio1);
+if (contador >= 15) {
 
-            contador++;
+System.out.println("--Memoria llena--");
 
-        }
+} else {
 
-    }
+Scanner entrada = new Scanner(System.in);
 
-    
+System.out.println("-------Contacto Nuevo-------");
 
-    }
+System.out.println("-Ingrese el nombre del contacto");
 
+String nombre1 = entrada.nextLine();
+
+System.out.println("-Ingrese su numero de telefono");
+
+String num_tel = entrada.nextLine();
+
+System.out.println("-ingrese su adomicilio-");
+
+String adomicilio1 = entrada.nextLine();
+
+miscontactos[contador] = new contacto(num_tel,nombre1, adomicilio1);
+
+contador++;
+
+}
+
+}
+
+static public String vercontacto(String nombre) {
+
+int posicion = -1;
+
+for (int i = 0; i < 10; i++) {
+
+if (miscontactos[i].vernum_tel().equals(nombre) && miscontactos != null) {
+
+posicion = i;
+
+break;
+
+}
+
+}
+
+if (posicion != -1) {
+
+return "contacto: " + miscontactos[posicion].vercontacto();
+
+} else {
+
+return "contacto no encontrado";
+
+}
+
+}
+
+}
 
